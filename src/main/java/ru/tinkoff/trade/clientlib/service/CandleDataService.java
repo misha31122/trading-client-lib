0967feab2d81e1afbf5799001a4f70a206c4aae0
misha31122 <50743077+misha31122@users.noreply.github.com>
2024-05-24 13:19:52 +0300

    TRADINGBOT-9 Сделать таблицу с результатами хранения подписок на свеч (#2)
    Co-authored-by: Жихарев Михаил Юрьевич (4071405) <zhiharevmu@vtb.ru>

--- src/main/java/ru/tinkoff/trade/clientlib/service/CandleDataService.java
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.trade.clientlib.service;
-
-import ru.tinkoff.piapi.contract.v1.MarketDataResponse;
-
-public interface CandleDataService {
-
-  void processHourCandles(MarketDataResponse marketDataResponse);
-
-}
