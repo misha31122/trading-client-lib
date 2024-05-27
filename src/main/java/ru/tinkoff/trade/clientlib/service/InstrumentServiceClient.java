e82a2dc9e23517a0f269d0503b439784289f5581
misha31122 <50743077+misha31122@users.noreply.github.com>
2024-05-27 16:06:57 +0300

    Tradingbot 9 (#3)
    * TRADINGBOT-9 Сделать таблицу с результатами хранения подписок на свеч
    
    * TRADINGBOT-9 Сделать таблицу с результатами хранения подписок на свеч
    
    ---------
    
    Co-authored-by: Жихарев Михаил Юрьевич (4071405) <zhiharevmu@vtb.ru>

--- src/main/java/ru/tinkoff/trade/clientlib/service/InstrumentServiceClient.java
+++ src/main/java/ru/tinkoff/trade/clientlib/service/InstrumentServiceClient.java
@@ -1,18 +1,9 @@
 package ru.tinkoff.trade.clientlib.service;
 
-import io.grpc.ChannelCredentials;
-import io.grpc.Grpc;
-import io.grpc.ManagedChannel;
-import io.grpc.TlsChannelCredentials;
-import io.grpc.netty.shaded.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.security.CallCredentialsHelper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
-import ru.tinkoff.piapi.contract.v1.InstrumentsServiceGrpc;
 import ru.tinkoff.piapi.contract.v1.InstrumentsServiceGrpc.InstrumentsServiceStub;
 
 @Slf4j
@@ -23,23 +14,5 @@ import ru.tinkoff.piapi.contract.v1.InstrumentsServiceGrpc.InstrumentsServiceStu
 @Service
 public class InstrumentServiceClient {
 
-  @Value("${tinkoff.token}")
-  private String token;
-  @Value("${tinkoff.grpc.client.host}")
-  private String host;
-  @Value("${tinkoff.grpc.client.port}")
-  private Integer port;
-  private InstrumentsServiceStub stub;
-
-  @PostConstruct
-  private void postConstruct() {
-    ChannelCredentials credentials = TlsChannelCredentials.newBuilder()
-        .trustManager(InsecureTrustManagerFactory.INSTANCE.getTrustManagers())
-        .build();
-    ManagedChannel channel = Grpc.newChannelBuilderForAddress(host, port, credentials)
-        .build();
-
-    this.stub = InstrumentsServiceGrpc.newStub(channel)
-        .withCallCredentials(CallCredentialsHelper.bearerAuth(token));
-  }
+  private final InstrumentsServiceStub stub;
 }
--- src/main/java/ru/tinkoff/trade/clientlib/service/SandBoxServiceClient.java
+++ src/main/java/ru/tinkoff/trade/clientlib/service/SandBoxServiceClient.java
@@ -1,18 +1,9 @@
 package ru.tinkoff.trade.clientlib.service;
 
-import io.grpc.ChannelCredentials;
-import io.grpc.Grpc;
-import io.grpc.ManagedChannel;
-import io.grpc.TlsChannelCredentials;
-import io.grpc.netty.shaded.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.security.CallCredentialsHelper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
-import ru.tinkoff.piapi.contract.v1.SandboxServiceGrpc;
 import ru.tinkoff.piapi.contract.v1.SandboxServiceGrpc.SandboxServiceStub;
 
 @Slf4j
@@ -23,24 +14,5 @@ import ru.tinkoff.piapi.contract.v1.SandboxServiceGrpc.SandboxServiceStub;
 @Service
 public class SandBoxServiceClient {
 
-  @Value("${tinkoff.token}")
-  private String token;
-  @Value("${tinkoff.grpc.client.host}")
-  private String host;
-  @Value("${tinkoff.grpc.client.port}")
-  private Integer port;
-  private SandboxServiceStub stub;
-
-  @PostConstruct
-  private void postConstruct() {
-    ChannelCredentials credentials = TlsChannelCredentials.newBuilder()
-        .trustManager(InsecureTrustManagerFactory.INSTANCE.getTrustManagers())
-        .build();
-    ManagedChannel channel = Grpc.newChannelBuilderForAddress(host, port, credentials)
-        .build();
-
-    this.stub = SandboxServiceGrpc.newStub(channel)
-        .withCallCredentials(CallCredentialsHelper.bearerAuth(token));
-  }
-
+  private final SandboxServiceStub stub;
 }
