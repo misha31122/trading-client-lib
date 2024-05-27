93fa5ff6d49a5295a52c88374c5a567ee7570844
Жихарев Михаил Юрьевич (4071405) <zhiharevmu@vtb.ru>
2024-05-27 10:33:05 +0300

    TRADINGBOT-9 Сделать таблицу с результатами хранения подписок на свеч

--- src/main/java/ru/tinkoff/trade/clientlib/service/MarketDataStreamServiceClient.java
+++ src/main/java/ru/tinkoff/trade/clientlib/service/MarketDataStreamServiceClient.java
@@ -3,25 +3,16 @@ package ru.tinkoff.trade.clientlib.service;
 import static ru.tinkoff.piapi.contract.v1.SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE;
 import static ru.tinkoff.piapi.contract.v1.SubscriptionAction.SUBSCRIPTION_ACTION_UNSUBSCRIBE;
 
-import io.grpc.ChannelCredentials;
-import io.grpc.Grpc;
-import io.grpc.ManagedChannel;
-import io.grpc.TlsChannelCredentials;
-import io.grpc.netty.shaded.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.grpc.stub.StreamObserver;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.security.CallCredentialsHelper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.piapi.contract.v1.CandleInstrument;
 import ru.tinkoff.piapi.contract.v1.MarketDataResponse;
 import ru.tinkoff.piapi.contract.v1.MarketDataServerSideStreamRequest;
-import ru.tinkoff.piapi.contract.v1.MarketDataStreamServiceGrpc;
 import ru.tinkoff.piapi.contract.v1.MarketDataStreamServiceGrpc.MarketDataStreamServiceStub;
 import ru.tinkoff.piapi.contract.v1.SubscribeCandlesRequest;
 import ru.tinkoff.piapi.contract.v1.SubscriptionInterval;
@@ -34,25 +25,7 @@ import ru.tinkoff.piapi.contract.v1.SubscriptionInterval;
 @Service
 public class MarketDataStreamServiceClient {
 
-  @Value("${tinkoff.token}")
-  private String token;
-  @Value("${tinkoff.grpc.client.host}")
-  private String host;
-  @Value("${tinkoff.grpc.client.port}")
-  private Integer port;
-  private MarketDataStreamServiceStub stub;
-
-  @PostConstruct
-  private void postConstruct() {
-    ChannelCredentials credentials = TlsChannelCredentials.newBuilder()
-        .trustManager(InsecureTrustManagerFactory.INSTANCE.getTrustManagers())
-        .build();
-    ManagedChannel channel = Grpc.newChannelBuilderForAddress(host, port, credentials)
-        .build();
-
-    this.stub = MarketDataStreamServiceGrpc.newStub(channel)
-        .withCallCredentials(CallCredentialsHelper.bearerAuth(token));
-  }
+  private final MarketDataStreamServiceStub stub;
 
 
   public void startGetCandlesStreamData(Set<String> figiSet,
