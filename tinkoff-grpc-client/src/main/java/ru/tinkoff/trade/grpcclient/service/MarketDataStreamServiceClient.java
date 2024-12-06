553ec69cfe493910b9987ef719c32455ba994acb
Mikhail Zhikharev <misha31122@mail.ru>
2024-12-06 11:49:39 +0300

    TRADINGBOT-9 Реализовать rest и grpc клиента в tinkoff

--- tinkoff-grpc-client/src/main/java/ru/tinkoff/trade/grpcclient/service/MarketDataStreamServiceClient.java
+++ tinkoff-grpc-client/src/main/java/ru/tinkoff/trade/grpcclient/service/MarketDataStreamServiceClient.java
@@ -7,14 +7,18 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.piapi.contract.v1.CandleInstrument;
 import ru.tinkoff.piapi.contract.v1.MarketDataResponse;
 import ru.tinkoff.piapi.contract.v1.MarketDataServerSideStreamRequest;
 import ru.tinkoff.piapi.contract.v1.MarketDataStreamServiceGrpc.MarketDataStreamServiceBlockingStub;
+import ru.tinkoff.piapi.contract.v1.OrderBookInstrument;
 import ru.tinkoff.piapi.contract.v1.SubscribeCandlesRequest;
+import ru.tinkoff.piapi.contract.v1.SubscribeOrderBookRequest;
 import ru.tinkoff.piapi.contract.v1.SubscriptionInterval;
 
 @Slf4j
@@ -25,7 +29,7 @@ public class MarketDataStreamServiceClient {
   private final MarketDataStreamServiceBlockingStub stub;
 
 
-  public Iterator<MarketDataResponse> startGetCandlesStreamData(Set<String> figiSet,
+  public Iterator<MarketDataResponse> startSubscribeCandles(Set<String> figiSet,
       SubscriptionInterval interval) {
 
     if (figiSet == null || figiSet.isEmpty()) {
@@ -47,7 +51,27 @@ public class MarketDataStreamServiceClient {
     return stub.marketDataServerSideStream(request);
   }
 
-  public void stopGetCandlesStreamData(Set<String> figiSet,
+  public Iterator<MarketDataResponse> startSubscribeOrderBook(String figi) {
+
+    if (StringUtils.isBlank(figi)) {
+      log.info("Figi set is empty");
+      return Collections.emptyIterator();
+    }
+
+    var request = MarketDataServerSideStreamRequest.newBuilder()
+        .setSubscribeOrderBookRequest(SubscribeOrderBookRequest.newBuilder()
+            .setSubscriptionAction(SUBSCRIPTION_ACTION_SUBSCRIBE)
+            .addAllInstruments(Stream.of(figi).map(figiValue -> OrderBookInstrument.newBuilder()
+                .setDepth(50)
+                .setInstrumentId(figiValue)
+                .build()).collect(Collectors.toSet()))
+            .build())
+        .build();
+
+    return stub.marketDataServerSideStream(request);
+  }
+
+  public void stopSubscribeCandles(Set<String> figiSet,
       SubscriptionInterval interval) {
 
     var request = MarketDataServerSideStreamRequest.newBuilder()
@@ -58,6 +82,21 @@ public class MarketDataStreamServiceClient {
                 .setInstrumentId(figi)
                 .build()).collect(Collectors.toSet()))
             .setWaitingClose(true)
+            .build())
+        .build();
+
+    stub.marketDataServerSideStream(request);
+  }
+
+  public void stopSubscribeOrderBook(String figi) {
+
+    var request = MarketDataServerSideStreamRequest.newBuilder()
+        .setSubscribeOrderBookRequest(SubscribeOrderBookRequest.newBuilder()
+            .setSubscriptionAction(SUBSCRIPTION_ACTION_UNSUBSCRIBE)
+            .addAllInstruments(Stream.of(figi).map(figiValue -> OrderBookInstrument.newBuilder()
+                .setDepth(50)
+                .setInstrumentId(figiValue)
+                .build()).collect(Collectors.toSet()))
             .build())
         .build();
 
